7761d303701dd24caa4ae1d33ad380f266c304bf
shibinreji <117815019+shibinreji@users.noreply.github.com>
2023-12-05 21:27:28 +0530

    Add files via upload

---
+++ inc/banner.inc.php
@@ -0,0 +1,2 @@
+<h1><a href="index.php">Educare</span></a></h1>
+<h3><a href="#">Hotline: +91 90618333462</a></h3>
---
+++ inc/footer.inc.php
@@ -0,0 +1,21 @@
+
+	<div class="footerMain">
+		<div class="content">
+			<div class="footer-grids">
+				<div class="footer one">
+					<h3>Created by</h3>
+					<p>Shibin Abraham Reji</p>
+					<p class="adam"></p>
+					<div class="clear"></div>
+				</div>
+				<div class="footer three">
+					<h3>Contact Information</h3>
+					<ul>
+						<li>+919061833462</li>
+						<li><a href="">shibinabrahamreji2001@gmail.com</a></li>
+					</ul>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
